f1713689cf6acbeb5b107c5439742e3f7299aa21
SandeepThakare <sandythakare9@gmail.com>
2018-07-24 12:08:24 +0530

    no done

--- src/lottery.js
+++ src/lottery.js
@@ -4,5 +4,5 @@ import solc from 'solc';
 
 const lotteryPath = path.resolve(__dirname, '../contracts', 'lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'UTF-8');
-export default solc.compile(sourceBuffer, 1).contracts[':Lottery'];
+export default solc.compile(source, 1).contracts[':Lottery'];
 // console.log(solc.compile(source, 1).contracts[':Lottery']);
--- test/lottery.test.js
+++ test/lottery.test.js
@@ -3,21 +3,32 @@ import ganache from 'ganache-cli';
 import Web3 from 'web3';
 const web3 = new Web3(ganache.provider());
 
-const { interface, bytecode } = require(../src/lottery.js);
-
+const lotteryData = require('../src/lottery.js');
+console.log("lotteryData ====> ", lotteryData);
 let lottery;
 let accounts;
+let contract;
 
 beforeEach(async() => {
     accounts = await web3.eth.getAccounts();
 
-    lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ date:bytecode })
+    // contract = web3.eth.Contract(lotteryData.default.interface);
+
+    lottery = await new web3.eth.Contract(JSON.parse(lotteryData.default.interface))
+    .deploy({ date : lotteryData.default.bytecode })
     .send({
-        from: accounts[0],
+        from: accounts[0],  
         gas: '1000000'
     })
+
+    // lottery = contract.new({
+    //     data: '0x' + lotteryData.default.bytecode,
+    //     from: account[0],
+    //     gas: '1000000'
+    // });
 });
+
+console.log("Lottery ====> ", lottery);
 
 describe('Lottery Contract Initialization', () => {
     it('deploys a contract', () => {
