96f2b3e89632a18c419005132414e9ecb2897db8
SandeepThakare <sandythakare9@gmail.com>
2018-07-25 12:17:51 +0530

    account added in contract

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -2,9 +2,9 @@ import assert from 'assert';
 import ganache from 'ganache-cli';
 import Web3 from 'web3';
 const web3 = new Web3(ganache.provider());
+import lotteryData from '../src/lottery.js'
+// console.log("lotteryData ====> ", lotteryData);
 
-const lotteryData = require('../src/lottery.js');
-console.log("lotteryData ====> ", lotteryData);
 let lottery;
 let accounts;
 let contract;
@@ -14,12 +14,14 @@ beforeEach(async() => {
 
     // contract = web3.eth.Contract(lotteryData.default.interface);
 
-    lottery = await new web3.eth.Contract(JSON.parse(lotteryData.default.interface)) //Interface is also called as ABI
-    .deploy({ date : lotteryData.default.bytecode })
+    lottery = await new web3.eth.Contract(JSON.parse(lotteryData.interface)) //Interface is also called as ABI
+    .deploy({ data : lotteryData.bytecode })
     .send({
         from: accounts[0],  
         gas: '1000000'
     });
+    
+    // console.log("Lottery ====> ", lottery);
 
     // lottery = contract.new({
     //     data: '0x' + lotteryData.default.bytecode,
@@ -28,10 +30,24 @@ beforeEach(async() => {
     // });
 });
 
-console.log("Lottery ====> ", lottery);
-
 describe('Lottery Contract Initialization', () => {
     it('deploys a contract', () => {
         assert.ok(lottery.options.address);
+    });
+
+    it('allows one account to enter', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        // console.log(players[0], accounts[0])
+
+        assert.equal(accounts[0], players[0], "first index of array should be equal");
+        assert.equal(1, players.length);
     });
 });
